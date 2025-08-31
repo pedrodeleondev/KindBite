c535ab325e801405c1965e2b9cb09e6a7c239737
Pedro <114964122+pedrodeleondev@users.noreply.github.com>
2025-08-30 21:30:26 -0600

    se borran archivos innecesarios

--- app/src/main/java/com/dappsm/kindbite/CUUsuariosScreen.kt
+++
@@ -1,7 +0,0 @@
-package com.dappsm.kindbite
-
-sealed class CUUsuariosScreen (val route:String) {
-    object CUDonaciones: CUUsuariosScreen("CUDonaciones")
-    object AdminUsuarios: CUUsuariosScreen("AdminUsuarios")
-
-}
